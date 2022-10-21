45bfdc6e1dcdb655192a1f1e6a73005095dd539f
ChanOSAnson <113829053+ChanOSAnson@users.noreply.github.com>
2022-10-21 21:57:08 +0800

    Update Cinema.java

--- MOBLIMA/Cinema.java
+++ MOBLIMA/Cinema.java
@@ -5,4 +5,33 @@ package MOBLIMA;
  */
 public class Cinema {
     
+    /**
+     * Name of cinema.
+     */
+    private String cinemaName;
+    
+    public Cinema (String Name) {
+        this.CinemaName = Name;
+    }
+    
+
+    /**
+     * This method sets a new name for the cinema
+     * 
+     * @param cinemaName is the new name for the cinema
+     */
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+    
+    
+    /**
+     * This method returns the name of the cinema
+     * 
+     * @return the name of the cinema
+     */
+    public String getCinemaName() {
+        return cinemaName;
+    }
+
 }
