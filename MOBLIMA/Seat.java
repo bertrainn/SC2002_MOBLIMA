bdf27b53477ec1d4c1a62616135a25c69c508c9d
ChanOSAnson <113829053+ChanOSAnson@users.noreply.github.com>
2022-10-21 14:46:22 +0800

    Update Seat.java

--- MOBLIMA/Seat.java
+++ MOBLIMA/Seat.java
@@ -6,28 +6,51 @@ public class Seat {
     private Ticket Ticket;
     private Constants.Status SeatStatus;
 
+    /**
+    * Constructor for Seats
+    */
     public Seat(int SeatID) {
         this.SeatID = SeatID;
         this.Ticket = null;
         this.SeatStatus = Constants.Status.Vacant;
     }
-
+    
+    
+    /**
+     * Returns SeatID.
+     */
     public int getSeatID() {
         return SeatID;
     }
-
+    
+    /**
+     * This method sets a new ticket for the seats of the movie
+     * 
+     * @param Ticket is the new Ticket for the seats
+     */
     public void setTicket(Ticket Ticket) {
         this.Ticket = Ticket;
     }
 
+    /**
+     * Returns ticket number.
+     */
     public Ticket getTicket() {
         return this.Ticket;
     }
 
+    /**
+     * This method sets a new title for the movie
+     * 
+     * @param title is the new title for the movie
+     */
     public void setSeatStatus(Constants.Status s) {
         this.SeatStatus = s;
     }
 
+    /**
+     * Returns seat status.
+     */
     public Constants.Status getSeatStatus() {
         return this.SeatStatus;
     }
