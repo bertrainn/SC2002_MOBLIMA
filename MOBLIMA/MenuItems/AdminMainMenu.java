7cd73de7d0ad97ba788fda9333534cb691b8491c
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-22 18:16:28 +0800

    Add files via upload

---
+++ MOBLIMA/MenuItems/AdminMainMenu.java
@@ -0,0 +1,16 @@
+package MOBLIMA.MenuItems;
+
+import static MOBLIMA.MenuItems.MenuMethods.*;
+
+public class AdminMainMenu {
+	
+	public void showMenu() {
+		printHeader("Admin");
+		printMenu("Choose from one of the following options:", 
+				  "1. Create/Update/Remove Movie Listing",
+				  "2. Create/Update/Remove cinema showtimes and movies to be shown",
+				  "3. Configure system settings",
+				  "4. Go back", "");
+	}
+	
+}
