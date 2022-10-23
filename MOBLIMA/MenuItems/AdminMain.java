459bdbf4ee8e5ac858ac5a91a5c962ef0b9aba79
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-23 11:43:37 +0800

    Add files via upload

---
+++ MOBLIMA/MenuItems/AdminMain.java
@@ -0,0 +1,29 @@
+package MOBLIMA.MenuItems;
+
+import static MOBLIMA.MenuItems.MenuMethods.*;
+
+public class AdminMain {
+
+	public static void main(String[] args) {
+	    amm = new AdminMainMenu();
+		int choice;
+		Scanner sc = new Scanner(System.in);
+		do {
+			amm.showMenu();
+			choice = sc.nextInt();
+		
+			switch (choice) {
+				case 1: editMovie();
+						break;
+				case 2: editCinema();
+						break;
+				case 3: configSetting();
+						break;
+				case 4: goBack();
+						break;
+			}
+		}while (choice < 4);
+		sc.close();
+	}
+	
+}
