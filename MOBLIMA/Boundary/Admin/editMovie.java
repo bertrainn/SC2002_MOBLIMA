63647e6b5804063606cc99d631fcf300c009e011
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-30 12:38:44 +0800

    Add files via upload

---
+++ MOBLIMA/Boundary/Admin/editMovie.java
@@ -0,0 +1,20 @@
+package MOBLIMA.Boundary.Admin;
+
+import static MOBLIMA.Entity.Cinema.*;
+
+public class editMovie {
+
+	    System.out.println("Movie code is %s and movie name is %s", getcinemaCode(), getCinemaName());
+	
+	
+		code = getStringInput("Please enter movie code: ");
+        setcinemaCode(code);
+        
+        name = getStringInput("Please enter movie name: ");
+
+	
+	    String name = userInput();
+        setCinemaName(name);
+        
+
+}
