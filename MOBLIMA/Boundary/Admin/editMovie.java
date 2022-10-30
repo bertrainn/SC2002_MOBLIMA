b63e340e05fd2c1d61915a42f1fdfa5be8fb0cb6
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-30 13:02:39 +0800

    Update editMovie.java

--- MOBLIMA/Boundary/Admin/editMovie.java
+++ MOBLIMA/Boundary/Admin/editMovie.java
@@ -4,25 +4,61 @@ import static MOBLIMA.Entity.Movie.*;
 
 public class editMovie {
 	
-	movie mov;
+	public static void main(String[] args) {
+		Movie mov = new Movie();
+		System.out.println("Movie title is %s and movie id is %s", mov.getTitle(), mov.getId());
+		
+		int choice;
+		Scanner sc = new Scanner(System.in);
+		do {
 
-	System.out.println("Movie title is %s and movie id is %s", mov.getTitle(), mov.getId());
+			choice = sc.nextInt();
+
+			switch (choice) {
+				case 1:
+					String title = getStringInput("Please enter movie title: ");
+        				mov.setTitle(title);
+					break;
+				case 2:
+					String des = getStringInput("Please enter movie description: ");
+        				mov.setDescriptoin(des);
+					break;
+				case 3:
+					String age = getStringInput("Please enter movie age rating: ");
+        				mov.setAgeRating(age);
+					break;
+				case 4:
+					ArrayList actor = getStringInput("Please enter movie actors: ");
+        				mov.setActor(actor);
+					break;
+				case 5:
+					ArrayList genre = getStringInput("Please enter movie genres: ");
+        				mov.setGenre(genre);
+					break
+				case 6:
+					ArrayList rev = getStringInput("Please enter movie reviews: ");
+       					mov.setReview(rev);	
+					break;
+
+			}
+		} while (choice < 4);
+		sc.close();
+	}
 	
-	String title = getStringInput("Please enter movie title: ");
-        mov.setTitle(title);
+	
+	
+
+	
+	
+	
         
-        String des = getStringInput("Please enter movie description: ");
-        mov.setDescriptoin(des);
         
-	String age = getStringInput("Please enter movie age rating: ");
-        mov.setAgeRating(age);
+        
 	
-	ArrayList actor = getStringInput("Please enter movie actors: ");
-        mov.setActor(actor);
 	
-	ArrayList genre = getStringInput("Please enter movie genres: ");
-        mov.setGenre(genre);
 	
-	ArrayList rev = getStringInput("Please enter movie reviews: ");
-        mov.setReview(rev);	
+	
+	
+	
+	
 }
