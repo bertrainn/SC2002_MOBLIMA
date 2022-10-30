763d936c334ee36542b2df5dbda5c938d014c1ff
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-30 13:04:15 +0800

    Update editCinema.java

--- MOBLIMA/Boundary/Admin/editCinema.java
+++ MOBLIMA/Boundary/Admin/editCinema.java
@@ -3,18 +3,34 @@ package MOBLIMA.Boundary.Admin;
 import static MOBLIMA.Entity.Cinema.*;
 
 public class editCinema {
-
-	    System.out.println("Movie code is %s and movie name is %s", getcinemaCode(), getCinemaName());
-	
-	
-		code = getStringInput("Please enter movie code: ");
-        setcinemaCode(code);
         
-        name = getStringInput("Please enter movie name: ");
-
+        name = getStringInput("Please enter movie name: ")
 	
-	    String name = userInput();
-        setCinemaName(name);
+	public static void main(String[] args) {
+		Cinema cin = new Cinema();
+		System.out.println("Movie code is %s and movie name is %s", getcinemaCode(), getCinemaName());
+		
+		int choice;
+		Scanner sc = new Scanner(System.in);
+		do {
+
+			choice = sc.nextInt();
+
+			switch (choice) {
+				case 1:
+					code = getStringInput("Please enter movie code: ");
+        				setcinemaCode(code);
+					break;
+				case 2:
+					String name = userInput();
+       					setCinemaName(name);
+					break;
+				
+			}
+		} while (choice < 3);
+		sc.close();
+	}
+	
         
 
 }
