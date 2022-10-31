2258b67dab827b5c514d8bc22ac095bb8cd9a624
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-31 11:39:15 +0800

    Update Admin.java

--- MOBLIMA/Entity/Admin.java
+++ MOBLIMA/Entity/Admin.java
@@ -1,5 +1,9 @@
 package MOBLIMA.Entity;
 
+/**
+ * Represents an instance of an administrator in the system.
+ */
+
 public class Admin extends User {
     public Admin(String Username, String Password) {
         super(Username, Password);
