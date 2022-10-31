dd8a43c8cf8897f8f9692817793d6fd6dde903d2
bertrainn <pokeflux@gmail.com>
2022-11-01 02:38:26 +0800

    Update editSettings.java

--- MOBLIMA/Boundary/Admin/editSettings.java
+++ MOBLIMA/Boundary/Admin/editSettings.java
@@ -6,6 +6,7 @@
  */
 
 package MOBLIMA.Boundary.Admin;
+
 import static MOBLIMA.Boundary.MenuMethods.*;
 
 import MOBLIMA.Boundary.BaseMenu;
@@ -16,10 +17,30 @@ public class editSettings extends BaseMenu {
 		// TODO Auto-generated method stub
 		printHeader("Edit Settings");
 		printMenu("Choose from one of the following options:",
-				"1. Search for a movie",
-				"2. List all movies",
-				"3. List the top 5 movies",
-				"4. Back");
+				"1. Add Holiday",
+				"2. Delete Holiday",
+				"3. List All Holidays",
+				"4. Change Movie Type Pricing",
+				"5. Change Cinema Type Pricing",
+				"6. Back");
+
+		int choice = userInput(1, 6);
+
+		switch (choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				navigate(this, new AdminMainMenu());
+				break;
+		}
 	}
 
 }
