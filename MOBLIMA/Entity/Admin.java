1e8191bc2a771b06573f59c1a0a302cc19f10558
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-10-31 11:43:51 +0800

    Update Admin.java

--- MOBLIMA/Entity/Admin.java
+++ MOBLIMA/Entity/Admin.java
@@ -5,6 +5,24 @@ package MOBLIMA.Entity;
  */
 
 public class Admin extends User {
+    
+    private String Username;
+    /**
+     * Username of administrator.
+     */
+    
+    private String Password;
+    /**
+     * Password of administrator.
+     */
+    
+    /**
+     * Constructor for the Table class, with additional parameters.
+     * 
+     * @param Username      name of administrator
+     * @param Password      password for administrator
+     */
+   
     public Admin(String Username, String Password) {
         super(Username, Password);
     }
