24fd42a6766a2b384555bc81f7b03c61d6837d0d
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-11-01 09:57:28 +0800

    Update Holiday.java

--- MOBLIMA/Entity/Holiday.java
+++ MOBLIMA/Entity/Holiday.java
@@ -3,32 +3,75 @@ package MOBLIMA.Entity;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+
+/**
+ * Represents an instance of a holiday used in the system.
+ */
+
 public class Holiday implements Serializable {
+    /**
+     * Name of holiday.
+     */
     private String name;
+    
+    /**
+     * Date of holiday.
+     */
     private LocalDate date;
 
+     /**
+     * Constructor for the Table class, with additional parameters.
+     * 
+     * @param name   name of holiday
+     * @param date   date of holiday
+     */
     public Holiday(String name, LocalDate date) {
         this.name = name;
         this.date = date;
 
     }
 
+    /**
+     * This method returns the name of holiday
+     * 
+     * @return the name of holiday
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * This method sets the name of holiday
+     * 
+     * @param id is the new name of holiday
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This method returns the date of holiday
+     * 
+     * @return the date of holiday
+     */
     public LocalDate getDate() {
         return this.date;
     }
 
+    /**
+     * This method returns the date in string format
+     * 
+     * @return the date in string format
+     */
     public String getDateString() {
         return this.date.format(Constants.dateFormatShort);
     }
 
+    /**
+     * This method sets the date of holiday
+     * 
+     * @param id is the new date of holiday
+     */
     public void setDate(LocalDate date) {
         this.date = date;
     }
