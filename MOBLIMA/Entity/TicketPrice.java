1ee7d003a666d5afe7cec638b198f58445d37762
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-11-02 12:51:07 +0800

    Update TicketPrice.java

--- MOBLIMA/Entity/TicketPrice.java
+++ MOBLIMA/Entity/TicketPrice.java
@@ -5,12 +5,20 @@ import MOBLIMA.Control.Holiday_Controller;
 import java.util.HashMap;
 import java.io.Serializable;
 
+/**
+ * Represents an instance of a ticket price used in the system.
+ */
+
 public class TicketPrice implements Serializable {
 
     public TicketPrice() {
 
     }
 
+    /**
+     * This method calculates the total price of the ticket based on the TicketType and
+     * MovieType
+     */
     public double calcPrice(Constants.MOVIE_TYPE movieType,
             Constants.TICKET_TYPE ticketType) {
         double totalvalue = getPrice_TicketType(ticketType) + getPrice_MovieType(movieType);
