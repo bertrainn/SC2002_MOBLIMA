fa592550393f28e6b09f6e9ad0b1119e3a98ed2c
shearestoh <103993847+shearestoh@users.noreply.github.com>
2022-11-02 12:53:34 +0800

    Update User.java

--- MOBLIMA/Entity/User.java
+++ MOBLIMA/Entity/User.java
@@ -2,27 +2,67 @@ package MOBLIMA.Entity;
 
 import java.io.Serializable;
 
+/**
+ * Represents an instance of a user used in the system.
+ */
+
 public class User implements Serializable {
+    
+    /**
+     * Username of user.
+     */
     private String username;
+    
+    /**
+     * Password of user.
+     */
     private String password;
 
+    
+    /**
+     * Constructor for the Ticket class, with additional parameters.
+     * 
+     * @param username   username of user
+     * @param password   password of user
+     */
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    
+    /**
+     * This method returns the username of user
+     * 
+     * @return the username of user
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * This method sets the username of user
+     * 
+     * @param id is the new username of user
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * This method returns the password of user
+     * 
+     * @return the password of user
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * This method sets the password of user
+     * 
+     * @param id is the new password of user
+     */
     public void setPassword(String password) {
         this.password = password;
     }
