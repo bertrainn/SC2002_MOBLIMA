3c7aba52c6de3b37fca65cc90fa730d9ba8781d0
yuezhonggg <101001775+yuezhonggg@users.noreply.github.com>
2022-11-05 20:54:18 +0800

    Update Cineplex_Controller.java

--- MOBLIMA/src/Control/Cineplex_Controller.java
+++ MOBLIMA/src/Control/Cineplex_Controller.java
@@ -12,8 +12,15 @@ import java.io.ObjectOutputStream;
 
 import java.util.ArrayList;
 
+/**
+ * Controller for the Movie Goers.
+ */
 public class Cineplex_Controller {
     public final static String FILENAME = "MOBLIMA/database/Cineplex.txt";
+    
+    /**
+     * Assigning each choices with a value.
+     */
     public final static int CHOICE_NAME = 0;
     public final static int CHOICE_CODE = 1;
     public final static int CHOICE_CINEMALIST = 2;
@@ -21,6 +28,12 @@ public class Cineplex_Controller {
     public Cineplex_Controller() {
     }
 
+    /**
+     * Function to read file. 
+     * @return A new arraylist that has the movie sessions.
+     * @exception IOException for the file
+     * @exception ClassNotFoundException for the file
+     */
     public ArrayList<Cineplex> readFile() {
         FileInputStream fis = null;
         ObjectInputStream ois = null;
@@ -39,6 +52,13 @@ public class Cineplex_Controller {
         return new ArrayList<Cineplex>();
     }
 
+    /**
+     * Function to add in a new cineplex.
+     * @param Name This cineplex's Name.
+     * @param CineplexCode This cineplex's code.
+     * @param CinemaList The cinemas to be added into the cineplex.
+     * @exception IOException for the reading the file
+     */
     public void addCineplex(String Name, String CineplexCode, ArrayList<Cinema> CinemaList) {
 
         FileOutputStream fos = null;
@@ -61,6 +81,12 @@ public class Cineplex_Controller {
         }
     }
 
+    /**
+     * Function that replaces the content in the file.
+     * @param data ArrayList that replaces the current data in the File.
+     * @param filename Name of the file.
+     * @exception IOException If there is any error within the file.
+     */
     public void replaceFile(ArrayList<Cineplex> data, String filename) {
         FileOutputStream fos = null;
         ObjectOutputStream oos = null;
@@ -79,6 +105,12 @@ public class Cineplex_Controller {
 
     }
 
+    /**
+     * Function that updates the cineplex's details.
+     * @param choice The choice of detail to change.
+     * @param Code The Cineplex's Code.
+     * @param obj The object that will update the choice of detail.
+     */
     public void updateCineplex(int choice, String Code, Object obj) {
         ArrayList<Cineplex> Data = readFile();
         ArrayList<Cineplex> UpdateData = new ArrayList<Cineplex>();
@@ -105,6 +137,10 @@ public class Cineplex_Controller {
         replaceFile(UpdateData, FILENAME);
     }
 
+    /**
+     * Function that will delete the cineplex.
+     * @param Code This Ciniplex's code.
+     */
     public void deleteCineplex(String Code) {
         ArrayList<Cineplex> Data = readFile();
         ArrayList<Cineplex> UpdateData = new ArrayList<Cineplex>();
@@ -119,6 +155,11 @@ public class Cineplex_Controller {
         replaceFile(UpdateData, FILENAME);
     }
 
+    /**
+     * Function that returns the cineplex using the cineplex code if exist
+     * @param cineplexCode The Cineplex's code.
+     * @return the cineplex if Cineplex exist otherwise null.
+     */
     public Cineplex getCineplexByCode(String cineplexCode) {
         ArrayList<Cineplex> cineplexList = readFile();
         for (Cineplex c : cineplexList) {
@@ -129,6 +170,11 @@ public class Cineplex_Controller {
         return null;
     }
 
+    /**
+     * Function that checks the cineplex existence by name.
+     * @param Name The Cineplex's Name.
+     * @return true if Cineplex exist otherwise false.
+     */
     public boolean CineplexExists(String Name) {
         ArrayList<Cineplex> Data = readFile();
 
@@ -140,6 +186,11 @@ public class Cineplex_Controller {
         return false;
     }
 
+    /**
+     * Function that checks the cineplex existence by code.
+     * @param Code The Cineplex's Code.
+     * @return true if Cineplex exist otherwise false.
+     */
     public boolean CineplexCodeExist(String Code) {
         ArrayList<Cineplex> Data = readFile();
 
