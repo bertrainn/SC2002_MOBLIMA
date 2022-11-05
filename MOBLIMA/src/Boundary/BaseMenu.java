5a8d40eb873efcf88921bb331472aa14660c59e2
yuezhonggg <101001775+yuezhonggg@users.noreply.github.com>
2022-11-05 21:48:31 +0800

    Update BaseMenu.java

--- MOBLIMA/src/Boundary/BaseMenu.java
+++ MOBLIMA/src/Boundary/BaseMenu.java
@@ -1,15 +1,22 @@
 package Boundary;
 
+/**
+ * This Abstract class is implemented for the login class and main menu class.
+ */
 public abstract class BaseMenu {
 	
 	//The menu before this menu
 	public BaseMenu previousMenu;
 	
-	//To be called when loading a new menu
+	/**
+	 * This method is to be called when loading a new menu.
+	 */
 	public abstract void load();
 	
-	//To be called when traversing back to a previous page
-	//If no previous page, then quit
+	/**
+	 * This method is to be called when traversing back to a previous page.
+	 * If no previous page, then quit.
+	 */
 	public void back() {
 		if (previousMenu == null) 
 			System.exit(1);
@@ -19,14 +26,18 @@ public abstract class BaseMenu {
 			}
 	}
 	
-	//Used to navigate to the next menu while saving current menu as next menu's prev
+	/**
+	 * This method is used to navigate to the next menu while saving current menu as next menu's previous
+	 */
 	public void navigate(BaseMenu a, BaseMenu b) {
 		b.previousMenu = a;
 		System.out.println();
 		b.load();
 	}
 	
-	//Gets the previous menu
+	/**
+	 * This method gets the previous menu.
+	 */
 	public BaseMenu getPrevMenu() {
 		return previousMenu;
 	}
