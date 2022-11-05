1ed86c5f3687f8ef9aa57d05e90eaa5b6cb98cc4
yuezhonggg <101001775+yuezhonggg@users.noreply.github.com>
2022-11-05 16:10:26 +0800

    Update UserInput_Controller.java

--- MOBLIMA/src/Control/UserInput_Controller.java
+++ MOBLIMA/src/Control/UserInput_Controller.java
@@ -9,17 +9,19 @@ import java.time.LocalDateTime;
 import Entity.Constants;
 
 /** 
- * Controller for User Input
- * @author Anon
- *
+ * Controller for User Input.
  */
 
 public class UserInput_Controller {
-
+	
 	static Scanner sc = new Scanner(System.in);
-
 	public static final DecimalFormat df = new DecimalFormat("0.00");
-
+	
+	/**
+	 * Function to print header.
+	 * @param header of userinput .
+	 */
+	
 	public static void printHeader(String header) {
 		int length = 65;
 		for (int i = 0; i < length; i++)
@@ -37,6 +39,11 @@ public class UserInput_Controller {
 		System.out.println();
 	}
 
+	/**
+	 * Function to print Menu .
+	 * @param menu of userinput .
+	 */
+	
 	public static void printMenu(String... menu) {
 		for (String s : menu) {
 			String firstHalf;
@@ -52,12 +59,23 @@ public class UserInput_Controller {
 		}
 		System.out.println();
 	}
+	
+	/**
+	 * Function to print Menu without spacing.
+	 * @param menu of userinput.
+	 */
 
 	public static void printMenuWithoutSpace(String... menu) {
 		for (String s : menu) {
 			System.out.println(s);
 		}
 	}
+	
+	/**
+	 * Function to generate spaces. 
+	 * @param size Size of the amount of spaces needed.
+	 * @return the string of spaces.
+	 */
 
 	public static String generateSpaces(int size) {
 		StringBuilder stringBuilder = new StringBuilder();
@@ -65,6 +83,16 @@ public class UserInput_Controller {
 			stringBuilder.append(" ");
 		return stringBuilder.toString();
 	}
+	
+	/**
+	 * Function to get the user choice.
+	 * Makes sure the choice does not have any error.
+	 * Makes sure the choice is with the range of choices.
+	 * @param i Value of the lowest choice.
+	 * @param j Value of the highest choice.
+	 * @return Choice of the user. 
+	 * @exception NumberFormatException On input error
+	 */
 
 	public static int userInput(int i, int j) {
 		int choice;
@@ -88,25 +116,52 @@ public class UserInput_Controller {
 
 		return choice;
 	}
+	
+	/**
+	 * Function to get the string input.
+	 * @param message Message to be printed.
+	 * @return the string of the next input.
+	 */
+	
 
 	public static String getStringInput(String message) {
 		System.out.print(message);
 		String input = sc.nextLine();
 		return input;
 	}
-
+	
+	/**
+	 * Function to get the string input in sentences
+	 * @param message Message to be printed.
+	 * @return the string of the next input.
+	 */
+	
 	public static String getStringInput_Sentence(String message) {
 		System.out.print(message);
 		String input = sc.nextLine();
 		return input;
 	}
-
+	
+	/**
+	 * Function to get the integer input 
+	 * @param message Message to be printed.
+	 * @return the integer of the next input.
+	 */
+	
 	public static int getIntInput(String message) {
 		System.out.print(message);
 		int input = Integer.parseInt(sc.nextLine());
 		return input;
 	}
 
+	/**
+	 * Function to get the integer which the integer 
+	 * have to be higher than the minimum value.
+	 * @param message Message to be printed.
+	 * @param min Minimum value that the Input integer have to be higher than.
+	 * @return the int of the next input.
+	 */
+	
 	public static int getIntInput_Min(String message, int min) {
 		System.out.print(message);
 		int input = Integer.parseInt(sc.nextLine());
@@ -116,12 +171,24 @@ public class UserInput_Controller {
 		}
 		return input;
 	}
+	
+	/**
+	 * Function to get the double input.
+	 * @param message Message to be printed in sentences.
+	 * @return the double of the next input.
+	 */
 
 	public static Double getDoubleInput(String message) {
 		System.out.print(message);
 		Double input = Double.parseDouble(sc.nextLine());
 		return input;
 	}
+	
+	/**
+	 * Function to get the date input. 
+	 * @param message Message to be printed.
+	 * @return the parsed date.
+	 */
 
 	public static LocalDate getDateInput(String message) {
 		System.out.print(message);
@@ -130,6 +197,12 @@ public class UserInput_Controller {
 		LocalDate parsedDate = LocalDate.parse(input, Constants.dateFormatLong);
 		return parsedDate;
 	}
+	
+	/**
+	 * Function to get the date input if there is no year.
+	 * @param message Message to be printed.
+	 * @return the parsed date.
+	 */
 
 	public static LocalDate getDateInput_NoYear(String message) {
 		System.out.print(message);
@@ -139,6 +212,12 @@ public class UserInput_Controller {
 		LocalDate parsedDate = LocalDate.parse(input, Constants.dateFormatLong);
 		return parsedDate;
 	}
+	
+	/**
+	 * Function to get the date and time input.
+	 * @param message Message to be printed.
+	 * @return the parsed date.
+	 */
 
 	public static LocalDateTime getDateTimeInput(String message) {
 		System.out.print(message);
@@ -147,6 +226,10 @@ public class UserInput_Controller {
 		LocalDateTime parsedDate = LocalDateTime.parse(input, Constants.datetimeFormat);
 		return parsedDate;
 	}
+	/**
+	 * Function to check the user and password of the admin account
+	 * @return Boolean indicating if the login is successful or a failure.
+	 */
 
 	public static boolean adminAuthorise() {
 		String _user = "GV";
@@ -161,6 +244,12 @@ public class UserInput_Controller {
 		else
 			return false;
 	}
+	
+	/**
+	 * Function to change the list to a string.
+	 * @param a List of strings to be converted to string.
+	 * @return the string of the converted list.
+	 */
 
 	public static String listToString(ArrayList<String> a) {
 		String s = "";
@@ -169,6 +258,13 @@ public class UserInput_Controller {
 		s = s + a.get(a.size() - 1);
 		return s;
 	}
+	
+	/**
+	 * Function to reduce the length of the string.
+	 * @param input String input that will be reduced.
+	 * @param maxLen The maximum length of the string allowed.
+	 * @return the reduced string
+	 */
 	
 	public static String reduceStringLength(String input, int maxLen) {
 		String res = input;
