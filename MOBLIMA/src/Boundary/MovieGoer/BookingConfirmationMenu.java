3d05670c319aa958dcd3aee62a41e1e4004b88eb
yuezhonggg <101001775+yuezhonggg@users.noreply.github.com>
2022-11-06 15:27:20 +0800

    Update BookingConfirmationMenu.java

--- MOBLIMA/src/Boundary/MovieGoer/BookingConfirmationMenu.java
+++ MOBLIMA/src/Boundary/MovieGoer/BookingConfirmationMenu.java
@@ -25,20 +25,48 @@ import Entity.MovieSession;
 import Entity.Seat;
 import Entity.SeatLayout;
 import Entity.Ticket;
-
+/**
+ * This shows the booking confirmation menu
+ */
 public class BookingConfirmationMenu extends BaseMenu {
 
+	/**
+	 * The MovieGoer who booked the tickets.
+	 */
 	private MovieGoer cust;
+	
+	/**
+	 * The Moviesession that was chosen by the moviegoer.
+	 */
 	private MovieSession ms;
+	
+	/**
+	 * The Cineplex that was chosen by the moviegoer.
+	 */
 	private Cineplex cp;
+	
+	/**
+	 * The Seats that was chosen by the moviegoer.
+	 */
 	private ArrayList<Seat> chosenSeats;
+	
+	/**
+	 * Intialising the controllers for the confirmed booking menu.
+	 */
 	private Cineplex_Controller cpc = new Cineplex_Controller();
 	private Cinema_Controller cc = new Cinema_Controller(cpc);
 	private MovieSession_Controller msc = new MovieSession_Controller(cc);
 	private Holiday_Controller hc = new Holiday_Controller();
 	private Booking_Controller bc = new Booking_Controller();
 	private MovieGoer_Controller mgc = new MovieGoer_Controller();
-
+	
+	/**
+         * Creates a new BookingConfirmationMenu with the given parameters.
+         * @param cust This BookingConfirmationMenu's MovieGoer.
+	 * @param ms This BookingConfirmationMenu's ms.
+	 * @param chosenSeats This BookingConfirmationMenu's chosenSeats.
+	 * @param cp This BookingConfirmationMenu's cp.
+         */
 	public BookingConfirmationMenu(MovieGoer cust, MovieSession ms, ArrayList<Seat> chosenSeats, Cineplex cp) {
 		this.cust = cust;
 		this.ms = ms;
@@ -46,6 +74,9 @@ public class BookingConfirmationMenu extends BaseMenu {
 		this.cp = cp;
 	}
 
+	/**
+         * Loads the Booking Confirmation Menu.
+         */
 	@Override
 	public void load() {
 		printHeader("Booking Confirmation");
@@ -70,6 +101,9 @@ public class BookingConfirmationMenu extends BaseMenu {
 		}
 	}
 
+	/**
+         * Loads the Booking Confirmation Menu.
+         */
 	private void showConfirmation() {
 		ArrayList<Ticket> tixList = new ArrayList<Ticket>();
 		int i = 1;
@@ -114,6 +148,13 @@ public class BookingConfirmationMenu extends BaseMenu {
 		}
 	}
 
+	/**
+         * Function creates ticket with given parameters.
+	 * @param session The ticket's movie session.
+	 * @param seat The ticket's seat number.
+	 * @param i [ Notsure whats the purpose of i ]
+	 * @return a completed ticket with the necessary details.
+         */
 	private Ticket createTicket(MovieSession session, Seat seat, int i) {
 		Constants.TICKET_TYPE ticketType = Constants.TICKET_TYPE.FRI_WEEKEND_PH;
 		Cinema cinema = cc.getCinemaByCode(session.getCinemaCode());
