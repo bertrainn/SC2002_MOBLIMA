955fcd9986511d5a5ecdeb16df36e49505d0a0dd
bertrainn <pokeflux@gmail.com>
2022-11-07 18:31:11 +0800

    added back button to ReviewView.()

--- MOBLIMA/src/Boundary/MovieGoer/ReviewView.java
+++ MOBLIMA/src/Boundary/MovieGoer/ReviewView.java
@@ -26,17 +26,18 @@ public class ReviewView extends BaseMenu {
 	 * The Movie of the review.
 	 */
 	private Movie m;
-	
+
 	/**
 	 * The MovieGoer who review the movie.
 	 */
 	private MovieGoer cust;
-	
+
 	/**
-	 * The Indicator on whether the moviegoer wants to view the movie review or leave a review..
+	 * The Indicator on whether the moviegoer wants to view the movie review or
+	 * leave a review..
 	 */
 	private boolean view;
-	
+
 	/**
 	 * Intialising the controllers for the confirmed booking menu.
 	 */
@@ -45,12 +46,13 @@ public class ReviewView extends BaseMenu {
 	private Review_Controller rc = new Review_Controller(mc, mgc);
 
 	/**
-         * Creates a new View review menu with the given parameters.
+	 * Creates a new View review menu with the given parameters.
 	 *
-         * @param m    This Review's Movie.
+	 * @param m    This Review's Movie.
 	 * @param mg   This Review's MovierGoer.
-	 * @param view The indicator on whether to view the movie review or leave a review.
-         */
+	 * @param view The indicator on whether to view the movie review or leave a
+	 *             review.
+	 */
 	public ReviewView(Movie m, MovieGoer mg, boolean view) {
 		this.m = m;
 		cust = mg;
@@ -58,8 +60,8 @@ public class ReviewView extends BaseMenu {
 	}
 
 	/**
-         * Loads the Review Menu.
-         */
+	 * Loads the Review Menu.
+	 */
 	@Override
 	public void load() {
 		if (view == true)
@@ -69,8 +71,8 @@ public class ReviewView extends BaseMenu {
 	}
 
 	/**
-         * Shows the Reviews of the requested movie.
-         */
+	 * Shows the Reviews of the requested movie.
+	 */
 	public void viewReviews() {
 		String tit = reduceStringLength(m.getTitle(), 35);
 		printHeader("Reviews for " + tit);
@@ -92,8 +94,8 @@ public class ReviewView extends BaseMenu {
 	}
 
 	/**
-         * Allows the MovieGoer to leave a review on the movie.
-         */
+	 * Allows the MovieGoer to leave a review on the movie.
+	 */
 	private void leaveReview() {
 		String tit = reduceStringLength(m.getTitle(), 35);
 		printHeader("Leave a Review for " + tit);
@@ -101,8 +103,9 @@ public class ReviewView extends BaseMenu {
 		if (cust == null) {
 			printMenu("You must be logged in to leave a review, choose one of the following options:",
 					"1. Login",
-					"2. Register");
-			int choice = userInput(1, 2);
+					"2. Register",
+					"3. Back");
+			int choice = userInput(1, 3);
 			switch (choice) {
 				case 1:
 					navigate(this, new LogIn(m));
@@ -110,6 +113,8 @@ public class ReviewView extends BaseMenu {
 				case 2:
 					navigate(this, new MovieGoerRegistration(m));
 					break;
+				case 3:
+					navigate(this, new MoviesList(cust));
 			}
 		} else {
 			int flag = 0;
